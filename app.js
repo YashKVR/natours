87b6a615727b33ae709a8af39bafacd92b1790c3
yashkvr <yashincrypto5@gmail.com>
2023-09-21 16:01:14 +0530

    created and added multiple routers

--- app.js
+++ app.js
@@ -104,6 +104,41 @@ const deleteTour = (req, res) => {
     })
 }
 
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+}
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+}
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+}
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+}
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+}
+
 //3] ROUTES
 /*
 app.get('/api/v1/tours', getAllTours);
@@ -113,14 +148,30 @@ app.patch('/api/v1/tours/:id', updateTour);
 app.delete('/api/v1/tours/:id', deleteTour);
 */
 
-app.route('/api/v1/tours')
-    .get(getAllTours)
-    .post(createTour)
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route('/api/v1/tours/:id')
+tourRouter.route('/')
+    .get(getAllTours)
+    .post(createTour);
+
+tourRouter.route('/:id')
     .get(getTour)
     .patch(updateTour)
-    .delete(deleteTour)
+    .delete(deleteTour);
+
+
+userRouter.route('/')
+    .get(getAllUsers)
+    .post(createUser)
+
+userRouter.route('/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser)
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4] START THE SERVER    
 const port = 3000;
