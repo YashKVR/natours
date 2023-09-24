510c02c87046c256c34db90eaf22228acf2f03d1
yashkvr <yashincrypto5@gmail.com>
2023-09-24 17:50:53 +0530

    Added multiple chaining middleware functions and serving static files functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,27 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is: ${val}`);
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+}
+
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'missing name or price'
+        })
+    }
+    next();
+}
+
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
@@ -20,14 +41,6 @@ exports.getTour = (req, res) => {
 
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
-
-    // if (id > tours.length) {
-    if (!tour) {
-        return res.status(404).json({
-            status: 'failed',
-            message: 'Invalid ID'
-        });
-    }
 
     res.status(200).json({
         status: 'success',
@@ -55,14 +68,6 @@ exports.createTour = (req, res) => {
 }
 
 exports.updateTour = (req, res) => {
-
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'failed',
-            message: 'Invalid ID'
-        });
-    }
-
     res.status(200).json({
         status: 'success',
         data: {
@@ -72,14 +77,6 @@ exports.updateTour = (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {
-
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'failed',
-            message: 'Invalid ID'
-        });
-    }
-
     res.status(204).json({
         status: 'success',
         data: null
