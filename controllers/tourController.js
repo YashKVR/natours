58fabf68571fc6b4dcd6e76e774c3ac2f66d9540
yashkvr <yashincrypto5@gmail.com>
2023-10-02 13:26:40 +0530

    Added filtering and advanced filtering to tours API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,34 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        console.log(req.query);
+
+        //BUILD THE QUERY
+        //1) FILTERING
+        const queryObj = { ...req.query }
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
+
+        //2)ADVANCED FILTERING
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log(JSON.parse(queryStr));
+
+        const query = Tour.find(JSON.parse(queryStr));
+
+
+        //EXECUTE THE QUERY
+        const tours = await query;
+
+
+
+        // const query = await Tour.find()
+        // .where('duration')
+        // .equals(5)
+        // .where('difficulty')
+        // .equals('easy');
+
+        //SEND RESPONSE
         res.status(200).json({
             status: 'success',
             results: tours.length,
