c65310dc5a28ff45c7b28b52154d9f446da798b3
yashkvr <yashincrypto5@gmail.com>
2023-10-03 13:35:49 +0530

    added sorting to tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,17 +5,24 @@ exports.getAllTours = async (req, res) => {
         console.log(req.query);
 
         //BUILD THE QUERY
-        //1) FILTERING
+        //1A) FILTERING
         const queryObj = { ...req.query }
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach(el => delete queryObj[el]);
 
-        //2)ADVANCED FILTERING
+        //1B)ADVANCED FILTERING
         let queryStr = JSON.stringify(queryObj)
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(queryStr));
 
-        const query = Tour.find(JSON.parse(queryStr));
+        let query = Tour.find(JSON.parse(queryStr));
+
+        // 2) Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query.sort('-createdAt');
+        }
 
 
         //EXECUTE THE QUERY
